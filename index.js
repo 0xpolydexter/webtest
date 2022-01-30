5ba0b05c5056526f8e8daf6c4ba19309a78dc837
Hitesh Gawhade <95847019+hiteshgawhade@users.noreply.github.com>
2022-01-30 18:49:16 +0530

    JS CWH1

---
+++ index.js
@@ -0,0 +1,12 @@
+//ways to print in JS
+console.log("Hello World");
+//alert("Hey There");
+document.write('this is document write');
+
+//JS console api
+console.log("Hello World", 4, "another log");
+console.warn('this is a warning');
+console.error('this is a warning')
+
+//JS Variables
+
