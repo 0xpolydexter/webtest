68cf6ecc9c8219eba2238afc8ef629083f2655f4
Hitesh Gawhade <95847019+hiteshgawhade@users.noreply.github.com>
2022-02-03 11:48:06 +0530

    till string reversal

--- Web/index.js
+++ Web/index.js
@@ -1,13 +1,12 @@
 //0,1,1,2,3,5,8,13,21,34,55
 // fibonacci using array
-
 var f = [];
 f[0] = 0;
 f[1] = 1;
 function fiboArray(a) {
     for (i = 2; i <= a; i++) {
         f[i] = f[i - 1] + f[i - 2];
-      //  console.log(f[i]);
+        //  console.log(f[i]);
         fx = f[i];
     }
     return fx;
@@ -17,27 +16,44 @@ console.log(fiboArray(10));
 //fibonacci using recursive
 function fiboRecursive(n) {
     return n < 1 ? 0
-            : n <= 2 ? 1 
-            : fiboRecursive(n-1)+fiboRecursive(n-2);
+        : n <= 2 ? 1
+            : fiboRecursive(n - 1) + fiboRecursive(n - 2);
 }
 console.log(fiboRecursive(10));
 
 //anagram strings
-//var str1 = 'fried';
-//var str2 = 'fired';
-var i,j;
-var x = false;
-var y = false;
-function checkAnagram(str1,str2)
-{
+var i, j;
+var z = 0;
+function checkAnagram(str1, str2) {
+    if (str1.length != str2.length) { return 'not anagram, lengths do not match!'; }
+    else {
+        for (i = 0; i < str1.length; i++) {
+            for (j = 0; j < str2.length; j++) {
+                if(str1[i] == str2[j]) {
+               // console.log('matches',str1[i],str2[j]);
+                z++;
+                }
+                }
+            }
+          //  console.log(z);
+            if (z>=str1.length)
+            return`${str1} and ${str2} are anagram!`;
+            else return `${str1} and ${str2} are not anagram!`;
+        }
+}
+console.log(checkAnagram('fired','fried'));
+console.log(checkAnagram('logo','ogol'));
+console.log(checkAnagram('abcd','abc'));
 
-for (i=0;i<str1.length;i++)
+//string reversal
+function reverseit(a)
 {
-    for(j=0;j<str2.length;j++)
-    {
-        if(str1[i]==str2[j])
-        x = true;
-    }
+    var b=[];
+for (i=0;i<a.length;i++)
+{
+    b[i] = a[a.length-i-1];
+    
 }
+return b.join("")
 }
-console.log(checkAnagram('X','XY'));
+console.log(reverseit('opportunity'));
